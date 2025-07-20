e49327f76a375de01e7f471efab319d0b482e156
Soyab Karki <SoyabKarki123@gmail.com>
2025-07-20 16:04:00 -0500

    Initialization

---
+++ backend/main.py
@@ -0,0 +1,14 @@
+from fastapi import FastAPI, UploadFile, File
+from fastapi.middleware.cors import CORSMiddleware
+import os
+
+app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
